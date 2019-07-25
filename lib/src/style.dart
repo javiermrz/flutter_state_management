1e41414ddd0c1dcbe0658c197d88a0082c8a24ee
javiermrz <javier.mrz@hotmail.com>
2019-07-25 12:21:26 +0200

    Color appSecondaryColor modificado

--- lib/src/style.dart
+++ lib/src/style.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/painting.dart';
 
-const Color appSecondaryColor = Color(0xff20242A);
 const Color appPrimaryColor = Color(0xfff5e2c6);
+const Color appSecondaryColor = Color(0xff2b303b);
 const Color appAccentColor = Color(0xffff662b);
